b401b18afa0975ec6601cd4052111992cd4de6bd
rayentrabelsi <101215132+rayentrabelsi@users.noreply.github.com>
2022-03-21 06:26:52 -0700

    Update minimap.h

--- minimap.h
+++ minimap.h
@@ -4,8 +4,9 @@
 typedef struct
 {
   SDL_Surface *img[3];
-  SDL_Rect posJoueur[3];
+  SDL_Rect[3];
 }minimap;
+
 void initminimap( minimap * m);
 void MAJMinimap(SDL_Rect posJoueur , minimap * m, SDL_Rect camera, int redimensionnement);
 void afficherminimap (minimap m, SDL_Surface * screen)
@@ -13,4 +14,4 @@ void sauvegarder( int score, char nomjoueur[], char nomfichier[]);
 void meilleur( char nomfichier[], int *score, char nomjoueur[]);
 
 
-#endif // MINIMAP_H_INCLUDED
+#endif // MINIMAP_H_INCLUDED 
